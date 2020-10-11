f2ce83427329c2c60fb177ccb5f99d6d3637b43b
pupsi4ek <sda2000den@gmail.com>
2020-10-11 07:32:11 +0300

    added map with markers

---
+++ src/components/Map.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="map">
+    <div id="mapContainer" style="height:600px;width:100%" ref="hereMap"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HereMap",
+  props: ['center', 'pointList'],
+  data() {
+    return {
+      platform: null,
+      apikey: "VPMeXhKfZ_R4DwoQujmGTPFLFoR2991A23QeCQUO8sY"
+    };
+  },
+  async mounted() {
+    const platform = new window.H.service.Platform({
+      apikey: this.apikey
+    });
+    this.platform = platform;
+    this.initializeHereMap();
+  },
+  methods: {
+    initializeHereMap() { // rendering map
+      const mapContainer = this.$refs.hereMap;
+      const H = window.H;
+      // Obtain the default map types from the platform object
+      let maptypes = this.platform.createDefaultLayers();
+      // Instantiate (and display) a map object:
+      let map = new H.Map(mapContainer, maptypes.vector.normal.map, {
+        zoom: 14,
+        center: this.center,
+        // pixelRatio: window.devicePixelRatio || 1
+      });
+
+      addEventListener("resize", () => map.getViewPort().resize());
+
+      // add behavior control
+      new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+      // add UI
+      H.ui.UI.createDefault(map, maptypes);
+
+      // End rendering the initial map
+      let LocationOfMarker = this.center;
+
+      // Create a marker icon from an image URL:
+      let icon = new H.map.Icon("https://www.flaticon.com/svg/static/icons/svg/25/25613.svg", { size: { w: 20, h: 20 } });
+
+      // Create a marker using the previously instantiated icon:
+      let marker = new H.map.Marker(LocationOfMarker, { icon: icon });
+
+      // Add the marker to the map:
+      map.addObject(marker);
+
+      for (let i in this.pointList){
+        let LocationOfMarker = this.pointList[i];
+        let marker = new H.map.Marker(LocationOfMarker, { icon: icon });
+        map.addObject(marker);
+      }
+    }
+  }
+};
+</script>
--- src/views/QuestPage.vue
+++ src/views/QuestPage.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="EventPage bg-gray-200 container mx-auto my-20 flex flex-row rounded-md">
     <div class="w-2/3 bg-white mr-12">
-          <img
-          class="w-full mb-4 rounded-md"
-          :src="require('../assets/images/' + quest.questImage)"
-          alt="Обложка мероприятия"
-          />
-          <div class="px-6">
-            <h1 class="font-bold text-4xl mb-5">
-              {{ quest.title }}
-            </h1>
-            <p class=" text-xl leading-relaxed">ID: {{ quest.id }}</p>
-            <p class=" text-xl leading-relaxed">
-              {{ quest.description }}
-            </p>
-          </div>
+      <!-- <img
+      class="w-full mb-4 rounded-md"
+      :src="require('../assets/images/' + quest.questImage)"
+      alt="Обложка мероприятия"
+      /> -->
+      <Map class="w-full mb-4 rounded-md" :center='center' :pointList='coordList'/>
+      <!-- <div v-for="mapPoint in quest.map" :key="mapPoint">
+        <p>{{ mapPoint }}</p>
+      </div> -->
+      <div class="px-6">
+        <h1 class="font-bold text-4xl mb-5">
+          {{ quest.title }}
+        </h1>
+        <p class=" text-xl leading-relaxed">ID: {{ quest.id }}</p>
+        <p class=" text-xl leading-relaxed">
+          {{ quest.description }}
+        </p>
+      </div>
     </div>
     <div class="RightBar w-1/3 bg-white h-full p-6 sticky top-2 rounded-md">
       <div class="Autour desc">
@@ -38,14 +42,37 @@
 
 <script>
 import { mapGetters } from "vuex";
+import Map from '@/components/Map'
 
 export default {
   name: "QuestPage",
+  components:{ Map },
+  data() {
+    return {
+      center:{ 
+        lat: 55.932791, 
+        lng: 37.439879
+      }
+    }  
+  },
   methods: {
   },
   created() {
     this.$store.dispatch("fetchQuest", this.$route.params.id);
   },
-  computed: mapGetters(["quest", "user"])
+  computed: {
+    ...mapGetters(["quest", "user"]),
+    coordList(){
+      let coords = []
+      for (let i in this.quest.map){
+        let arr = this.quest.map[i].split(',')
+        let obj = {}
+        obj['lat'] = parseFloat(arr[0])
+        obj['lng'] = parseFloat(arr[1])
+        coords.push(obj)
+      }
+      return coords
+    }
+  }
 };
 </script>
